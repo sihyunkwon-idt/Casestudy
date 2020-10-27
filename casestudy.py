9d5b46b9388ffcdb228b40b775ac2ece436cad3f
sihyunkwon <71270479+sihyunkwon@users.noreply.github.com>
2020-10-27 14:49:10 -0400

    Update casestudy.py

--- casestudy.py
+++ casestudy.py
@@ -12,3 +12,11 @@ health.columns = ['year','Indicator','Country','value']
 health = health.pivot(index=['Country','year'], columns='Indicator')
 
 health = health.droplevel(level=0, axis=1).reset_index()
+
+
+# Bar Plot for "Missing Value by an Indicator"
+missing_by_indi =  (health.isna().sum() / len(health)).sort_values()[2:]   
+plt.figure(figsize=(20,10))
+sns.barplot(x='Indicator', y=0, data=missing_by_indi.reset_index())
+plt.tick_params(axis='x', bottom=False, labelbottom=False)
+plt.ylabel('% of Missing')
